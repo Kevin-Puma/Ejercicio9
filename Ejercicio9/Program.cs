eb363d0413f4ba5073803db8482c291b18ce6079
User <User@DESKTOP-CRJD420>
2023-08-28 23:51:56 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio9/Program.cs
@@ -0,0 +1,29 @@
+﻿ Console.WriteLine("Ingrese el primer número:");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número:");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el tercer número:");
+int numero3 = int.Parse(Console.ReadLine());
+
+if (numero1 != numero2 && numero1 != numero3 && numero2 != numero3)
+{
+int mayor = numero1;
+
+if (numero2 > mayor)
+{
+mayor = numero2;
+}
+
+if (numero3 > mayor)
+{
+mayor = numero3;
+}
+
+Console.WriteLine("El mayor de los tres números es: " + mayor);
+}
+else
+{
+Console.WriteLine("Los números deben ser diferentes.");
+}
